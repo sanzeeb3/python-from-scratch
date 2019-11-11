f066b2501e3fe9cddf871beb3268f754bab626c4
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-11-11 09:34:03 +0545

    Comments in python

--- home.py
+++ home.py
@@ -1,7 +1,10 @@
+#Printing Hello World.
 print("Hello World")
 
+#Condition check. Intendation is used to indicate a block of code. Intendation in python is not only for readability.
 if 5 > 2:
     print("Five is greater than two!")
 
+# Variables.
 x = 190
 print(x)
